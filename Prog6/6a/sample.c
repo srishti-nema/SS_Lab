e90baf8c63935bebf17fc0f2b8e5efa569a0f503
srishti-nema <srishtinema.0699@gmail.com>
2020-06-17 01:47:15 +0530

    initial commit

---
+++ Prog6/6a/sample.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	//sum of two numbers
+	int a,b,c;
+	printf("Enter the values of a, b\n");
+	scanf("%d%d", &a, &b);
+	c=a+b;
+	printf("Sum=%d\n",c);
+	return 0;
+	/*end of program*/
+}
+
